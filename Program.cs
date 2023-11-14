4ea6db776efce45c08ff49f5e114ba3f81c367c0
[eliza1h] <[hodinitaeliza2@gmail.com]>
2023-11-14 17:31:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_obiecte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
